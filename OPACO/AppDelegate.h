96307b7927b4c4d8d04c440e9919e64c73bfedd4
mdp01 <miguel.alc@gmail.com>
2013-03-20 18:33:47 -0500

    Initial Commit

---
+++ OPACO/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OPACO
+//
+//  Created by usuario on 20/03/13.
+//  Copyright (c) 2013 usuario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
